9f95bd53c66ea89247dde8b5563b036e84d5e347
thomas <thomas.chiantia@gmail.com>
2017-08-22 17:17:12 -0400

    shorter var name

--- src/urlp.c
+++ src/urlp.c
@@ -21,66 +21,61 @@
 
 #include "urlp.h"
 
-int urlp_encoder_init(urlp_encoder *encoder, uint8_t *bytes, uint32_t size) {
-    memset(encoder, 0, sizeof(urlp_encoder));
-    encoder->size = encoder->spot = size;
-    encoder->bytes = bytes;
+int urlp_encoder_init(urlp_encoder *enc, uint8_t *bytes, uint32_t size) {
+    memset(enc, 0, sizeof(urlp_encoder));
+    enc->size = enc->spot = size;
+    enc->bytes = bytes;
     return 0;
 }
 
-uint32_t urlp_item(urlp_encoder *encoder, uint8_t *src, uint32_t slen) {
+uint32_t urlp_item(urlp_encoder *enc, uint8_t *src, uint32_t slen) {
     uint32_t size;
-    if (!encoder->spot) return 0;
+    if (!enc->spot) return 0;
     if (slen == 0) {
 	size = 1;
-	encoder->bytes[encoder->spot--] = 0x80u;
+	enc->bytes[enc->spot--] = 0x80u;
     } else if (slen == 1 && src[0] < 0x80) {
 	size = 1;
-	encoder->bytes[encoder->spot--] = src[0];
+	enc->bytes[enc->spot--] = src[0];
     } else if (slen <= 55) {
 	size = slen + 1;
-	if (encoder->spot - size < 0) return 0;  // negative result possible?
-	encoder->spot -= size;
-	encoder->bytes[encoder->spot] = 0x80 + slen;
-	memcpy(&encoder->bytes[encoder->spot + 1], src, slen);
+	if (enc->spot - size < 0) return 0;  // negative result possible?
+	enc->spot -= size;
+	enc->bytes[enc->spot] = 0x80 + slen;
+	memcpy(&enc->bytes[enc->spot + 1], src, slen);
     } else {
 	int szsz = 4 - (urlp_clz_fn(slen) / 8);
 	size = szsz + 1 + slen;
 	uint8_t(*b)[4] = (uint8_t(*)[4])(&slen);
-	if (encoder->spot - size < 0) return 0;  // negative result possible?
-	encoder->spot -= size;
-	encoder->bytes[encoder->spot] = 0xb7 + szsz;
+	if (enc->spot - size < 0) return 0;  // negative result possible?
+	enc->spot -= size;
+	enc->bytes[enc->spot] = 0xb7 + szsz;
 	for (int i = 0; i < szsz; i++) {
 	    // Portable ?
-	    encoder->bytes[encoder->spot + 1 + i] = *b[i];
+	    enc->bytes[enc->spot + 1 + i] = *b[i];
 	}
-	memcpy(&encoder->bytes[encoder->spot + 1 + szsz], src, slen);
+	memcpy(&enc->bytes[enc->spot + 1 + szsz], src, slen);
     }
     return size;
 }
 
-uint32_t urlp_list(urlp_encoder *encoder, int n, ...) {
-    uint32_t bytes = urlp_size(encoder);
+uint32_t urlp_list(urlp_encoder *enc, int n, ...) {
+    uint32_t bytes = urlp_size(enc);
     if (bytes <= 55) {
-	encoder->bytes[--encoder->spot] = 0xc0 + bytes;
+	enc->bytes[--enc->spot] = 0xc0 + bytes;
     } else {
 	// Portable ?
 	int szsz = 4 - (urlp_clz_fn(bytes) / 8);
 	uint8_t(*b)[4] = (uint8_t(*)[4])(&bytes);
 	for (int i = 0; i < szsz; i++) {
-	    encoder->bytes[encoder->spot + 1 + i] = *b[i];
+	    enc->bytes[enc->spot + 1 + i] = *b[i];
 	}
     }
     return bytes;
 }
 
-uint32_t urlp_size(urlp_encoder *encoder) {
-    return encoder->size - encoder->spot;
-}
-
-uint8_t *urlp_data(urlp_encoder *encoder) {
-    return &encoder->bytes[encoder->spot];
-}
+uint32_t urlp_size(urlp_encoder *enc) { return enc->size - enc->spot; }
+uint8_t *urlp_data(urlp_encoder *enc) { return &enc->bytes[enc->spot]; }
 
 //
 //
