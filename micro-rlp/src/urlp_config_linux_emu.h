bcf2e00fb0060018b9a1e5e78e5e957c8122d991
thomas <thomas.chiantia@gmail.com>
2017-08-31 09:17:08 -0400

    reorg

--- micro-rlp/src/urlp_config_linux_emu.h
+++ micro-rlp/src/urlp_config_linux_emu.h
@@ -1,12 +1,6 @@
 #ifndef URLP_CONFIG_LINUX_EMU_H_
 #define URLP_CONFIG_LINUX_EMU_H_
 
-//#include <assert.h>
-//#include <stdarg.h>
-//#include <stdbool.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdlib.h>
