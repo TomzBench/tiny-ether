f3db89f47bb6f313613bfdfaa6552dd1cd7fb4a2
thomas <thomas.chiantia@gmail.com>
2017-08-30 22:08:59 -0400

    cleaned makefile

--- common.mk
+++ common.mk
@@ -5,3 +5,23 @@ MKDIR_P:= mkdir -p
 
 ${dirs}:
 	${MKDIR_P} ${dirs}
+
+${LIBDIR}/%.so: ${dirs} ${relobj-y} ${hdrs}
+	@echo "LINK $@"
+	@${CC} -shared ${relobj-y} ${LDFLAGS} -o $@
+
+${LIBDIR}/%.a: ${dirs} ${obj-y} ${hdrs}
+	@echo "LINK $@"
+	@ar rcs $@ ${obj-y}
+
+${INCDIR}/${BRAND}/%.h: ${SRCDIR}/%.h
+	@echo "COPY $@"
+	@cp $< $@
+
+./obj/%.lo: ${SRCDIR}/%.c
+	@echo "  CC $@"
+	@${CC} -c -fPIC ${CFLAGS} ${LDFLAGS} $< -o $@ 
+
+./obj/%.o: ${SRCDIR}/%.c
+	@echo "  CC $@"
+	@${CC} -c ${CFLAGS} ${LDFLAGS} $< -o $@ 
