5875d7d426da052a179fcfc886cd2dce213594d3
thomas <thomas.chiantia@gmail.com>
2017-09-07 17:08:48 -0400

    ...

--- libucrypto/mpi.c
+++ libucrypto/mpi.c
@@ -1,14 +1,1 @@
 #include "mpi.h"
-#include "string.h"
-
-int
-ucrypto_mpi_xor(ucrypto_mpi* dst,
-                const ucrypto_mpi* src,
-                uint8_t* bytes,
-                size_t l)
-{
-    //if (!(l <= dst->n)) return -1;
-    //if (!(l <= src->n)) return -1;
-    for (uint32_t i = 0; i < l; i++) dst->p[i] ^= bytes[i];
-    return 0;
-}
