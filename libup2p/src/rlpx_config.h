098d963f72b3c6e43264d93fd6e8138586af93a9
thomas <thomas.chiantia@gmail.com>
2017-09-08 12:49:01 -0400

    ...:

--- libup2p/src/rlpx_config.h
+++ libup2p/src/rlpx_config.h
@@ -1,7 +1,9 @@
 #ifndef URLPX_CONFIG_COMMON_H_
 #define URLPX_CONFIG_COMMON_H_
 
-#ifdef URLPX_CONFIG_LINUX_EMU
+#ifdef URLPX_CONFIG_FILE
+#include URLPX_CONFIG_FILE
+#elif URLPX_CONFIG_LINUX_EMU
 #include "rlpx_config_linux_emu.h"
 #endif
 
