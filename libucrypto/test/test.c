4a832cac82551339b7f37e36f842a602a9159a27
thomas <thomas.chiantia@gmail.com>
2017-09-09 23:10:35 -0400

    fixed test vectors

--- libucrypto/test/test.c
+++ libucrypto/test/test.c
@@ -1,13 +1,20 @@
 #include "mtm/ecc.h"
 #include <string.h>
 
-int test_ecdh();
+/**
+ * @brief Test vectors
+ */
 const char* alice_key_str =
-    "5e173f6ac3c669587538e7727cf19b782a4f2fda07c1eaa662c593e5e85e3051";
+    "19c2185f4f40634926ebed3af09070ca9e029f2edd5fae6253074896205f5f6c";
 const char* bob_key_str =
-    "c45f950382d542169ea207959ee0220ec1491755abe405cd7498d6b16adb6df8";
+    "d25688cf0ab10afa1a0e2dba7853ed5f1e5bf1c631757ed4e103b593ff3f5620";
 const char* expect_secret_str =
-    "e3f407f83fc012470c26a93fdff534100f2c6f736439ce0ca90e9914f7d1c381";
+    "E3F407F83FC012470C26A93FDFF534100F2C6F736439CE0CA90E9914F7D1C381";
+
+/**
+ * @brief Prototypes
+ */
+int test_ecdh();
 
 int
 main(int argc, char* argv[])
@@ -47,7 +54,8 @@ test_ecdh()
     err |= ucrypto_mpi_cmp(secreta, secretb) ? -1 : 0;
     if (!(err == 0)) goto EXIT;
     ucrypto_mpi_write_string(&ctxa.z, 16, secret_str, l, &l);
-    printf("%s\n\n", secret_str);
+    err = memcmp(expect_secret_str, secret_str, strlen(secret_str)) ? -1 : 0;
+    if (!(err == 0)) goto EXIT; // note our write fn prints in caps
 
     // Generate shared secret with known private keys with binary public key
     ucrypto_ecc_ptob(&ctxa, &pubkeya);
