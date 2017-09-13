b6b1e76eecb451089a8359f390d65b06a0f34e96
thomas <thomas.chiantia@gmail.com>
2017-09-13 09:37:32 -0400

    format change

--- libucrypto/mpi.h
+++ libucrypto/mpi.h
@@ -7,45 +7,16 @@ extern "C" {
 
 #include "mbedtls/bignum.h"
 
-typedef struct
-{
-    uint8_t b[256];
-} b2048;
-
-typedef struct
-{
-    uint8_t b[128];
-} b1024;
-
-typedef struct
-{
-    uint8_t b[65];
-} b520;
-
-typedef struct
-{
-    uint8_t b[64];
-} b512;
-
-typedef struct
-{
-    uint8_t b[32];
-} b256;
-
-typedef struct
-{
-    uint8_t b[20];
-} b160;
-
-typedef struct
-{
-    uint8_t b[16];
-} b128;
-
-typedef struct
-{
-    uint8_t b[8];
-} b64;
+// clang-format off
+typedef struct { uint8_t b[256] ;} b2048;
+typedef struct { uint8_t b[128] ;} b1024;
+typedef struct { uint8_t b[65]  ;} b520;
+typedef struct { uint8_t b[64]  ;} b512;
+typedef struct { uint8_t b[32]  ;} b256;
+typedef struct { uint8_t b[20]  ;} b160;
+typedef struct { uint8_t b[16]  ;} b128;
+typedef struct { uint8_t b[8]   ;} b64;
+// clang-format on
 
 typedef mbedtls_mpi ucrypto_mpi; /*!< multi precesion integer */
 
