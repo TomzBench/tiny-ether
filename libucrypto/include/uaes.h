8bc6642c93bc3e64c8de34bd94384a31e9997c13
thomas <thomas.chiantia@gmail.com>
2017-09-25 14:41:59 -0400

    replaced macros with static inlines for type checking

--- libucrypto/include/uaes.h
+++ libucrypto/include/uaes.h
@@ -14,16 +14,8 @@ typedef struct{uint8_t b[16];} uaes_iv;
 
 typedef mbedtls_aes_context uaes_ctx;
 
-#define uaes_init_bin(ctx, key, keylen) uaes_init(ctx, keylen * 8, key)
-#define uaes_init_128(ctx, key) uaes_init(ctx, 128, key)
-#define uaes_init_256(ctx, key) uaes_init(ctx, 256, key)
 int uaes_init(uaes_ctx* ctx, int keysz, uint8_t* key);
 void uaes_deinit(uaes_ctx** ctx);
-
-#define uaes_crypt_ctr_128(k, iv, in, l, o)                                    \
-    uaes_crypt_ctr(128, (k)->b, (iv)->b, in, l, o)
-#define uaes_crypt_ctr_256(k, iv, in, l, o)                                    \
-    uaes_crypt_ctr(256, (k)->b, (iv)->b, in, l, o)
 int uaes_crypt_ctr(int keysz,
                    uint8_t* key,
                    uint8_t* iv,
@@ -35,6 +27,48 @@ int uaes_crypt_ctr_op(uaes_ctx* ctx,
                       const uint8_t* in,
                       size_t inlen,
                       uint8_t* out);
+
+/**
+ * @brief Inline aliaes api wrapper
+ */
+
+static inline int
+uaes_init_bin(uaes_ctx* ctx, uint8_t* key, size_t keylen)
+{
+    return uaes_init(ctx, keylen * 8, key);
+}
+
+static inline int
+uaes_init_128(uaes_ctx* ctx, uint8_t* key)
+{
+    return uaes_init(ctx, 128, key);
+}
+
+static inline int
+uaes_init_256(uaes_ctx* ctx, uint8_t* key)
+{
+    return uaes_init(ctx, 256, key);
+}
+
+static inline int
+uaes_crypt_ctr_128(uaes_ctr_128_key* key,
+                   uaes_iv* iv,
+                   const uint8_t* in,
+                   size_t inlen,
+                   uint8_t* out)
+{
+    return uaes_crypt_ctr(128, key->b, iv->b, in, inlen, out);
+}
+
+static inline int
+uaes_crypt_ctr_256(uaes_ctr_128_key* key,
+                   uaes_iv* iv,
+                   const uint8_t* in,
+                   size_t inlen,
+                   uint8_t* out)
+{
+    return uaes_crypt_ctr(256, key->b, iv->b, in, inlen, out);
+}
 
 #ifdef __cplusplus
 }
