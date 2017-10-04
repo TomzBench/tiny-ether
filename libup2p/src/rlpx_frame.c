a4d721465c87248d13a304f11aea84f148413f09
thomas <thomas.chiantia@gmail.com>
2017-10-03 23:47:04 -0400

    added better frame logic to determine if frame is a header or frame

--- libup2p/src/rlpx_frame.c
+++ libup2p/src/rlpx_frame.c
@@ -1,10 +1,5 @@
 #include "rlpx_frame.h"
 
-// FIXME - frame-egress and frame-ingress currently assume all frames are > 16
-// bytes.  note here is reminder fix that. Otherwise a small body frame will
-// trick into encoding like a header when it is a frame.  Multi frames can
-// easily have < 16 bytes...
-
 int frame_egress(rlpx* s,
                  const uint8_t* x,
                  size_t xlen,
@@ -54,7 +49,7 @@ rlpx_frame_write_rlp(rlpx* s,
     memset(head, 0, 32);
     WRITE_BE(3, head, (uint8_t*)&rlplen);
     head[3] = '\xc2', head[4] = '\x80' + type, head[5] = '\x80' + id;
-    frame_egress(s, head, 16, out, &out[16]);
+    frame_egress(s, head, 0, out, &out[16]);
     frame_egress(s, body, totlen, &out[32], &out[32 + totlen]);
     return 0;
 }
@@ -109,7 +104,7 @@ rlpx_frame_parse_header(rlpx* s,
     int err = -1;
     uint8_t temp[32];
 
-    err = frame_ingress(s, header, 16, &header[16], temp);
+    err = frame_ingress(s, header, 0, &header[16], temp);
     if (err) return err;
 
     // Read in big endian length prefix, give to caller
@@ -154,14 +149,17 @@ frame_egress(rlpx* s, const uint8_t* x, size_t xlen, uint8_t* out, uint8_t* mac)
     // FRAME:
     // egress-mac.update(aes(mac-secret,egress-mac) ^
     // left128(egress-mac.update(frame-ciphertext).digest))
+    //
+    // if xlen == 0 x is a header else x is a frame
     uint8_t xin[32], tmp[32];
     memset(xin, 0, 32);
-    if (uaes_crypt_ctr_update(&s->aes_enc, x, xlen, out)) return -1;
-    if (xlen > 16) { // TODO test with header flag instead of packet let
+    if (xlen) {
+        if (uaes_crypt_ctr_update(&s->aes_enc, x, xlen, out)) return -1;
         ukeccak256_update(&s->emac, (uint8_t*)out, xlen);
         ukeccak256_digest(&s->emac, xin);
     } else {
-        memcpy(xin, out, xlen);
+        if (uaes_crypt_ctr_update(&s->aes_enc, x, 16, out)) return -1;
+        memcpy(xin, out, 16);
     }
     ukeccak256_digest(&s->emac, tmp);          // egress-mac
     uaes_crypt_ecb_enc(&s->aes_mac, tmp, tmp); // aes(mac-secret,egress-mac)
@@ -186,13 +184,15 @@ frame_ingress(rlpx* s,
     // FRAME: (ingress frame ciphertext before calling))
     // ingres-mac.update(aes(mac-secret,ingres-mac) ^
     // left128(ingres-mac.update(frame-ciphertext).digest))
+    //
+    // if xlen == 0 x is a header else x is a frame
     uint8_t tmp[32], xin[32];
     memset(xin, 0, 32);
-    if (xlen > 16) { // TODO test with header flag instead of packet let
+    if (xlen) {
         ukeccak256_update(&s->imac, (uint8_t*)x, xlen);
         ukeccak256_digest(&s->imac, xin);
     } else {
-        memcpy(xin, x, xlen);
+        memcpy(xin, x, 16);
     }
     ukeccak256_digest(&s->imac, tmp);          // ingress-mac
     uaes_crypt_ecb_enc(&s->aes_mac, tmp, tmp); // aes(mac-secret,ingress-mac)
@@ -200,7 +200,7 @@ frame_ingress(rlpx* s,
     ukeccak256_update(&s->imac, tmp, 16);      // ingress(...)
     ukeccak256_digest(&s->imac, tmp);          // ingress(...).digest
     if (memcmp(tmp, expect, 16)) return -1;    // compare expect with actual
-    if (uaes_crypt_ctr_update(&s->aes_dec, x, xlen, out)) return -1;
+    if (uaes_crypt_ctr_update(&s->aes_dec, x, xlen ? xlen : 16, out)) return -1;
     return 0;
 }
 
