f9ab206efdbf3f4a835feb38796e54bdf37ed207
thomas <thomas.chiantia@gmail.com>
2017-10-18 19:21:04 -0400

    added todo

--- cmake/default.cmake
+++ cmake/default.cmake
@@ -12,9 +12,10 @@ option(UETH_USE_SECP256K1 "Link with libsecp256k1.a" ON)
 add_definitions(-DURLP_CONFIG_UNIX)
 add_definitions(-DUSYS_CONFIG_UNIX)
 
+# TODO - use the libsecp256k1 repo add a cmake.txt
 if(UETH_USE_SECP256K1)
 	ExternalProject_Add(secp256k1
 		INSTALL_DIR ${CMAKE_SOURCE_DIR}/target
-		GIT_SUBMODULES external/secp256k1
+		GIT_REPOSITORY https://github.com/chfast/secp256k1
 	)
 endif()
