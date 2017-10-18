989c6a11e7b91698470fbb9f9ab844f1d8788d86
thomas <thomas.chiantia@gmail.com>
2017-10-18 19:18:19 -0400

    added cmake external projects

--- cmake/default.cmake
+++ cmake/default.cmake
@@ -11,3 +11,10 @@ option(UETH_USE_SECP256K1 "Link with libsecp256k1.a" ON)
 # TODO depreciate these?
 add_definitions(-DURLP_CONFIG_UNIX)
 add_definitions(-DUSYS_CONFIG_UNIX)
+
+if(UETH_USE_SECP256K1)
+	ExternalProject_Add(secp256k1
+		INSTALL_DIR ${CMAKE_SOURCE_DIR}/target
+		GIT_SUBMODULES external/secp256k1
+	)
+endif()
