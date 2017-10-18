15c514e3898f78fc8246a69a8a8f19d630591c48
thomas <thomas.chiantia@gmail.com>
2017-10-18 14:41:45 -0400

    moved some files around

--- cmake/default.cmake
+++ cmake/default.cmake
@@ -1,5 +1,11 @@
 #default build unix host unix target
 
+# libusys objs
 set(UETH_TARGET_OS UNIX)
+
+# libucrypto config
+set(UETH_USE_LIBSECP256K1 TRUE)
+
+# TODO depreciate these?
 add_definitions(-DURLP_CONFIG_UNIX)
 add_definitions(-DUSYS_CONFIG_UNIX)
