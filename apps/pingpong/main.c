62e35831806d235ccc0b4b5559cf61beab35f918
thomas <thomas.chiantia@gmail.com>
2017-10-27 13:55:24 -0400

    ...

--- apps/pingpong/main.c
+++ apps/pingpong/main.c
@@ -4,11 +4,11 @@
 #include "usys_time.h"
 
 #define SKEY "5e173f6ac3c669587538e7727cf19b782a4f2fda07c1eaa662c593e5e85e3051"
+#define REMOTE                                                                 \
+    "ca634cae0d49acb401d8a4c6b6fe8c55b70d115bf400769cc1400f3258cd31387574077f" \
+    "301b421bc84df7266c44e9e6d569fc56be00812904767bf5ccd1fc7f"
 
-const char* g_test_enode = "enode://"
-                           "ca634cae0d49acb401d8a4c6b6fe8c55b70d115bf400769cc14"
-                           "00f3258cd31387574077f301b421bc84df7266c44e9e6d569fc"
-                           "56be00812904767bf5ccd1fc7f@127.0.0.1:30303.0";
+const char* g_test_enode = "enode://" REMOTE "@127.0.0.1:30303";
 
 ueth_config config = { //
     .p2p_private_key = SKEY,
