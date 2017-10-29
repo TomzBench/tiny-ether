88a97e95c44fdb73647cc3a1e7dfa940ca1ad578
thomas <thomas.chiantia@gmail.com>
2017-10-28 22:17:57 -0400

    discovery write method stubs

--- libup2p/rlpx_discovery.h
+++ libup2p/rlpx_discovery.h
@@ -17,6 +17,20 @@ typedef enum {
     RLPX_DISCOVERY_NEIGHBOURS = 4
 } RLPX_DISCOVERY;
 
+typedef struct
+{
+    uint8_t ip[16]; /*!< BE encoding ipv4 or ipv3 data */
+    uint32_t tcp;   /*!< devp2p port */
+    uint32_t udp;   /*!< p2p port */
+} rlpx_discovery_endpoint;
+
+typedef struct
+{
+    uecc_public_key nodeid;     /*!< pubkey */
+    rlpx_discovery_endpoint ep; /*!< remote endpoint routing*/
+    urlp* meta;                 /*!< meta data (topics?) */
+} rlpx_discovery_node;
+
 int rlpx_discovery_recv(usys_sockaddr* addr, const uint8_t* b, uint32_t l);
 int rlpx_discovery_parse(usys_sockaddr* addr,
                          const uint8_t* b,
@@ -26,9 +40,25 @@ int rlpx_discovery_parse(usys_sockaddr* addr,
                          urlp** rlp);
 
 int rlpx_discovery_parse_ping(usys_sockaddr* addr, const urlp** rlp);
+int rlpx_discovery_print_ping(uint32_t ver,
+                              uint32_t timestamp,
+                              const rlpx_discovery_endpoint* ep_src,
+                              const rlpx_discovery_endpoint* ep_dst,
+                              uint8_t* dst,
+                              uint32_t* l);
 int rlpx_discovery_parse_pong(usys_sockaddr* addr, const urlp** rlp);
+int rlpx_discovery_print_pong(uint32_t timestamp,
+                              h256* echo,
+                              const rlpx_discovery_endpoint* ep_to,
+                              uint8_t* d,
+                              uint32_t* l);
 int rlpx_discovery_parse_find(usys_sockaddr* addr, const urlp** rlp);
+int rlpx_discovery_print_find(uint8_t* nodeid,
+                              uint32_t timestamp,
+                              uint8_t* b,
+                              uint32_t* l);
 int rlpx_discovery_parse_neighbours(usys_sockaddr* addr, const urlp** rlp);
+//  rlpx_discovery_print_neighbours( ....TODO
 
 #ifdef __cplusplus
 }
